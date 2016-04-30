ca2b34112c7666c93b371fb87370381a2e115a3e
Vyacheslav Bessonov <v.bessonov@hotmail.com>
2016-05-01 00:51:34 +0600

    Add WorkerParameter

---
+++ GZip/GZip.Core/Workers/WorkerParameter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VBessonov.GZip.Core.Workers
+{
+    public class WorkerParameter<T>
+    {
+        public T Parameter { get; set; }
+
+        public CancellationToken CancellationToken { get; set; }
+    }
+}
