d46b62f1cbfd217df424eb155e34d8f4d373fdfd
Vyacheslav Bessonov <v.bessonov@hotmail.com>
2016-05-01 00:43:46 +0600

    Add SystemInfo API

---
+++ GZip/GZip.Core/WinApi/ProcessorArchitecture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VBessonov.GZip.Core.WinApi
+{
+    internal enum ProcessorArchitecture
+    {
+        X86 = 0,
+        X64 = 9,
+        @Arm = -1,
+        Itanium = 6,
+        Unknown = 0xFFFF,
+    }
+}
---
+++ GZip/GZip.Core/WinApi/SystemInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace VBessonov.GZip.Core.WinApi
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SystemInfo
+    {
+        public ProcessorArchitecture ProcessorArchitecture;
+        public uint PageSize;
+        public IntPtr MinimumApplicationAddress;
+        public IntPtr MaximumApplicationAddress;
+        public IntPtr ActiveProcessorMask;
+        public uint NumberOfProcessors;
+        public uint ProcessorType;
+        public uint AllocationGranularity;
+        public ushort ProcessorLevel;
+        public ushort ProcessorRevision;
+
+        [DllImport("kernel32.dll", SetLastError = false)]
+        private static extern void GetSystemInfo(out SystemInfo info);
+
+        public static SystemInfo Current
+        {
+            get
+            {
+                SystemInfo systemInfo;
+
+                SystemInfo.GetSystemInfo(out systemInfo);
+
+                return systemInfo;
+            }
+        }
+    }
+}
