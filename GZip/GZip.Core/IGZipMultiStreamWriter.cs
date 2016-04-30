4537df3fe7609186149897abb3933405f00a3ef6
Vyacheslav Bessonov <v.bessonov@hotmail.com>
2016-05-01 01:53:36 +0600

    Remove redundant code

--- GZip/GZip.Core/IGZipMultiStreamWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace VBessonov.GZip.Core
-{
-    interface IGZipMultiStreamWriter
-    {
-        void Write(GZipMultiStreamCollection multiStreamCollection, Stream outputStream);
-    }
-}
