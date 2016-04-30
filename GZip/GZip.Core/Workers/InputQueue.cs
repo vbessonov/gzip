936de786f083087a5cce1d70fd526991645c5c0c
Vyacheslav Bessonov <v.bessonov@hotmail.com>
2016-05-01 00:48:23 +0600

    Add InputQueue and OutputQueue

---
+++ GZip/GZip.Core/Workers/InputQueue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace VBessonov.GZip.Core.Workers
+{
+    internal class InputQueue : SynchronizedCollection<CompressionInputWorkItem>
+    {
+
+    }
+}
---
+++ GZip/GZip.Core/Workers/OutputQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace VBessonov.GZip.Core.Workers
+{
+    internal class OutputQueue : SynchronizedKeyedCollection<int, CompressionOutputWorkItem>
+    {
+        private readonly AutoResetEvent _event = new AutoResetEvent(false);
+
+        private readonly int _inputSize;
+
+        public int InputSize
+        {
+            get { return _inputSize; }
+        }
+
+        public OutputQueue(int inputSize)
+        {
+            if (inputSize < 0)
+            {
+                throw new ArgumentOutOfRangeException("Input size must be non-negative integer");
+            }
+
+            _inputSize = inputSize;
+        }
+
+        public EventWaitHandle Event
+        {
+            get { return _event; }
+        }
+
+        protected override int GetKeyForItem(CompressionOutputWorkItem item)
+        {
+            return item.OutputStream.Index;
+        }
+
+        protected override void InsertItem(int index, CompressionOutputWorkItem item)
+        {
+            base.InsertItem(index, item);
+
+            _event.Set();
+        }
+
+        protected override void SetItem(int index, CompressionOutputWorkItem item)
+        {
+            base.SetItem(index, item);
+
+            _event.Set();
+        }
+    }
+}
