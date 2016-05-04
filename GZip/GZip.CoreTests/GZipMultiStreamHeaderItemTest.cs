52b82b1b5870e58a9c56ba40ed2ee26ccd3053bd
Vyacheslav Bessonov <v.bessonov@hotmail.com>
2016-05-04 19:37:08 +0600

    Fix tests

--- GZip/GZip.CoreTests/GZipMultiStreamHeaderItemTest.cs
+++ GZip/GZip.CoreTests/GZipMultiStreamHeaderItemTest.cs
@@ -10,30 +10,22 @@ namespace VBessonov.GZip.CoreTests
         [TestMethod]
         public void TestSerialize()
         {
-            ulong offset = ushort.MaxValue;
-            ushort length = ushort.MaxValue;
+            long length = long.MaxValue;
             GZipMultiStreamHeaderItem item = new GZipMultiStreamHeaderItem
             {
                 Length = length
             };
             byte[] buffer = item.Serialize();
 
-            Assert.AreEqual(offset, BitConverter.ToUInt64(buffer, 0));
-            Assert.AreEqual(length, BitConverter.ToUInt16(buffer, sizeof(ulong)));
+            Assert.AreEqual(length, BitConverter.ToInt64(buffer, 0));
         }
 
         [TestMethod]
         public void TestDeserialize()
         {
-            ulong offset = ushort.MaxValue;
-            ushort length = ushort.MaxValue;
+            long length = long.MaxValue;
             GZipMultiStreamHeaderItem item = new GZipMultiStreamHeaderItem();
-            byte[] offsetBytes = BitConverter.GetBytes(offset);
-            byte[] lengthBytes = BitConverter.GetBytes(length);
-            byte[] buffer = new byte[offsetBytes.Length + lengthBytes.Length];
-
-            offsetBytes.CopyTo(buffer, 0);
-            lengthBytes.CopyTo(buffer, offsetBytes.Length);
+            byte[] buffer = BitConverter.GetBytes(length);
 
             item.Deserialize(buffer);
 
@@ -43,8 +35,7 @@ namespace VBessonov.GZip.CoreTests
         [TestMethod]
         public void TestCanDeserializeSerializedItem()
         {
-            ushort offset = ushort.MaxValue;
-            ushort length = ushort.MaxValue;
+            long length = long.MaxValue;
             GZipMultiStreamHeaderItem item = new GZipMultiStreamHeaderItem
             {
                 Length = length
